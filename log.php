ef41a44de8e538013a36b5f7ea91db8800da61f0
eurekasolution <kjmin@daum.net>
2024-12-06 15:43:10 +0900

    E1544

--- log.php
+++ log.php
@@ -7,6 +7,38 @@
     // 53 : 6
     // n  : ceil(n/10)
 
+    if(isset($_GET["mode"]) and $_GET["mode"] == "add")
+    {
+        $ip = $_GET["ip"];
+        $sql = "select * from black where ip='$ip' ";
+        $result = mysqli_query($conn, $sql);
+        $data = mysqli_fetch_array($result);
+
+        if($data)
+        {
+            // 데이터 있으니까, 해제
+            $delSql = "delete from black where ip='$ip'";
+            $delResult = mysqli_query($conn, $delSql);
+            $msg = "해제 되었습니다. $ip";
+
+            //echo "$delSql<br>";
+        }else
+        {
+            // 블랙리스트에 추가
+            $inSql = "insert into black (ip, num) values('$ip', '0')";
+            $inResult = mysqli_query($conn, $inSql);
+            $msg = "등록 되었습니다. $ip";
+            //echo "$inSql<br>";
+        }
+
+        echo "
+        <script>
+            alert('$msg');
+            location.href='index.php?cmd=log';
+        </script>
+        ";
+    }
+
     $sql = "select * from log ";
     $result = mysqli_query($conn, $sql);
     $dataCount = mysqli_num_rows($result);
@@ -78,6 +110,14 @@
         
     }
 ?>
+    <script>
+        function blackControl(ip)
+        {
+            location.href='index.php?cmd=log&mode=add&ip='+ip
+            //alert(ip);
+        }
+    </script>
+
     <div class="row">
         <table class="table table-bordered">
             <tr>
@@ -94,6 +134,16 @@
                 {
                     $nation = ip2nation($data["ip"]);
                     $nationalFlag = "<img src='flags/$nation.gif'>";
+
+                    $bSql = "select * from black where ip='$data[ip]' ";
+                    $bResult = mysqli_query($conn, $bSql);
+                    $bData = mysqli_fetch_array($bResult);
+
+                    if($bData)
+                        $printButton = "<button type='button' class='btn btn-danger btn-sm' onClick=\"blackControl('$data[ip]')\">해제</button>";
+                    else
+                        $printButton = "<button type='button' class='btn btn-success btn-sm' onClick=\"blackControl('$data[ip]')\">차단</button>";
+                    
                     echo"
                     <tr>
                         <td>$data[idx]</td>
@@ -102,7 +152,9 @@
                         <td>$data[id]</td>
                         <td>$data[time]</td>
                         <td>$nationalFlag</td>
-                        <td>비고</td>
+                        <td>
+                            $printButton
+                        </td>
                     </tr>";
                     $data = mysqli_fetch_array($result);
                 }
